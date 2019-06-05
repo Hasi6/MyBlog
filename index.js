52bc1f33dc7a8b44737129ec23a8f2de8f130445
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-06-05 18:38:14 +0530

    Create ui

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+const path = require('path');
 const express = require('express');
 
 const port = 5500;
 
 const app = new express();
+
+app.use(express.static('public'));
+
+//Home Page
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+// About Page
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+// SamplePost Page
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+});
+
+// Contact Page
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
 
 app.listen(port, () => {
     console.log(`App Start on ${port}`);
