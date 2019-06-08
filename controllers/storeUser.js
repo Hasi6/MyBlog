d063d135db22089da0a71af98abeb3b06df5c984
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-06-08 22:20:49 +0530

    validate the user register form
    Now if the users didn't fill any input field they will return to the register page and data didn't store in database and if users fill all fields then they redirect to the home page and data will store in the database

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -3,6 +3,10 @@ const User = require('../database/models/User');
 
 module.exports = (req, res) => {
     User.create(req.body, (err, user) => {
+        if (err) {
+            return res.redirect('/auth/register');
+        }
         res.redirect('/');
+
     })
 };
--- database/models/User.js
+++ database/models/User.js
@@ -1,9 +1,34 @@
+// to hash user password before save in the database
+const bcrypt = require('bcrypt');
+
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
+    username: {
+        type: String,
+        required: true, //check weather if user enterend username or not
+        unique: true //check if the same username is already in the database
+    },
+    email: {
+        type: String,
+        required: true, //check weather if user enterend email or not
+        unique: true, //check if the same email is already in the database
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
+
+// Before save in database run the function in here
+// UserSchema.pre('save', function(next) {
+//     const user = this
+
+//     bcrypt.hash(user.password, 10, (err, encrypted) => {
+//         user.password = encrypted;
+//         console.log(err);
+//         next();
+//     }); //20 is the number of the length of the encryption password if the number is high the process time also long
+// });
 
 module.exports = mongoose.model('User', UserSchema);
